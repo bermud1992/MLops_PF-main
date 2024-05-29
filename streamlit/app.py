b38b4195a1bc4027c1549bb86a12b8ef1e8765bb
Sebastian Mariño <70497951+marinho14@users.noreply.github.com>
2024-05-28 19:54:50 -0500

    change api and streamlit

--- streamlit/app.py
+++ streamlit/app.py
@@ -17,46 +17,22 @@ def main():
     st.title("Aplicación de Streamlit para API POST")
 
     # Definir los valores por defecto basados en el esquema
-    default_values = {       
-        'race': 'Caucasian',
-        'gender': 'Female',
-        'age': '[80-90)',
-        'admission_type_id': 1,
-        'discharge_disposition_id': 1,
-        'admission_source_id': 7,
-        'metformin': 'Steady',
-        'repaglinide': 'No',
-        'nateglinide': 'No',
-        'chlorpropamide': 'No',
-        'glimepiride': 'No',
-        'acetohexamide': 'No',
-        'glipizide': 'No',
-        'glyburide': 'Steady',
-        'tolbutamide': 'No',
-        'pioglitazone': 'No',
-        'rosiglitazone': 'No',
-        'acarbose': 'No',
-        'miglitol': 'No',
-        'troglitazone': 'No',
-        'tolazamide': 'No',
-        'insulin': 'No',
-        'glyburide_metformin': 'No',
-        'glipizide_metformin': 'No',
-        'metformin_rosiglitazone': 'No',
-        'metformin_pioglitazone': 'No',
-        'change': 'Ch',
-        'diabetesMed': 'Yes',
-        'diag_1': 435,
-        'diag_2': 491,
-        'diag_3': 428,
-        'time_in_hospital': 10,
-        'num_lab_procedures': 45,
-        'num_procedures': 0,
-        'num_medications': 14,
-        'number_outpatient': 0,
-        'number_emergency': 0,
-        'number_inpatient': 0,
-        'number_diagnoses': 9
+    default_values = {
+        'brokered_by': 8103.0,
+        'status': 'sold',
+        'price': 375900.0,
+        'bed': 3.0,
+        'bath': 1.0,
+        'acre_lot': 1.2,
+        'street': 1467938.0,
+        'city': 'Kennett Square',
+        'state': 'Pennsylvania',
+        'zip_code': 19348.0,
+        'house_size': 1995.0,
+        'prev_sold_date': '2022-01-21',
+        'batch_number': 4,
+        'max_batch_number': 4,
+        'last_batch_number': 4
     }
 
     # Crear campos de entrada para cada atributo
@@ -65,9 +41,11 @@ def main():
         input_values[key] = st.text_input(key, value)
 
     # Convertir valores a los tipos de datos correctos
-    request_body = {key: [int(value)] if key not in ['race', 'gender', 'age', 'metformin', 'repaglinide', 'nateglinide', 'chlorpropamide', 'glimepiride', 'acetohexamide'
-        ,'glipizide', 'glyburide', 'tolbutamide', 'pioglitazone', 'rosiglitazone', 'acarbose', 'miglitol', 'troglitazone', 'tolazamide', 'insulin', 'glyburide_metformin'
-        ,'glipizide_metformin', 'metformin_rosiglitazone','metformin_pioglitazone', 'change', 'diabetesMed'] else [value] for key, value in input_values.items()}
+    string_columns = ['status', 'city', 'state', 'prev_sold_date']
+    request_body = {
+        key: [int(value)] if key not in string_columns else [value]
+        for key, value in input_values.items()
+    }
 
     if st.button("Realizar Predicción"):
         st.write("Realizando predicción...")
