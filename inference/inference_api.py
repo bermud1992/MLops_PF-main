db29f43214757d60d1e6407a3f6c6b1e32e214c8
Santiago_Bermudez <bermud1992@gmail.com>
2024-05-29 15:22:33 -0500

    update mlflow , airflow, fastapi, streamlit

--- inference/inference_api.py
+++ inference/inference_api.py
@@ -1,6 +1,7 @@
 import joblib
 import sklearn
 from fastapi import FastAPI, HTTPException , APIRouter
+from mlflow.client import MlflowClient
 import uvicorn
 from pydantic import BaseModel
 from typing import List
@@ -11,20 +12,15 @@ import mlflow
 class PropertyDetails(BaseModel):
     brokered_by: List[float] = [8103.0]
     status: List[str] = ['sold']
-    price: List[float] = [375900.0]
-    bed: List[float] = [3.0]
-    bath: List[float] = [1.0]
-    acre_lot: List[float] = [1.2]
-    street: List[float] = [1467938.0]
     city: List[str] = ['Kennett Square']
     state: List[str] = ['Pennsylvania']
     zip_code: List[float] = [19348.0]
+    bed: List[float] = [3.0]
+    bath: List[float] = [1.0]
+    acre_lot: List[float] = [1.2]
     house_size: List[float] = [1995.0]
-    prev_sold_date: List[str] = ['2022-01-21']
-    batch_number: List[float] = [4]
-    max_batch_number: List[float] = [4]
-    last_batch_number: List[float] = [4]
-
+    price: List[float] = [375900.0]
+    
 app = FastAPI()
 
 def decode_input(input):
@@ -33,23 +29,30 @@ def decode_input(input):
     model_columns = [
         'brokered_by', 
         'status', 
-        'price', 
-        'bed', 
-        'bath', 
-        'acre_lot', 
-        'street', 
         'city', 
         'state', 
         'zip_code', 
-        'house_size', 
-        'prev_sold_date', 
-        'batch_number', 
-        'max_batch_number', 
-        'last_batch_number'
+        'bed', 
+        'bath', 
+        'acre_lot', 
+        'house_size',
+        'price'  
     ]
     df = df[model_columns]
     print(df)
     return df
+
+def load_model_with_tag(experiment_name, tag_key, tag_value):
+    client = MlflowClient()
+    experiment = mlflow.get_experiment_by_name(experiment_name)
+    runs = client.search_runs([experiment.experiment_id], filter_string=f"tags.{tag_key} = '{tag_value}'")
+    if not runs:
+        raise Exception(f"No se encontró ningún modelo con el tag {tag_key} = {tag_value}")
+    # Asumimos que solo hay un modelo con este tag, tomar el último run
+    last_run = runs[0]
+    model_uri = f"runs:/{last_run.info.run_id}/model"
+    model = mlflow.pyfunc.load_model(model_uri)
+    return model
 
 @app.post("/predict/{model_name}")
 def predict_model(input_data : PropertyDetails,model_name: str = "modelo_base"):
@@ -58,9 +61,9 @@ def predict_model(input_data : PropertyDetails,model_name: str = "modelo_base"):
     os.environ['AWS_SECRET_ACCESS_KEY'] = 'supersecret'
     mlflow.set_tracking_uri("http://mlflow:5000")
     #mlflow.set_experiment("mlflow_tracking_examples")
-    model_production_uri = "models:/{model_name}/production".format(model_name=model_name)
-    #loaded_model = mlflow.pyfunc.load_model(model_uri=model_production_uri)
-    loaded_model = mlflow.pyfunc.load_model(f"models:/{model_name}@production")    
+    model_production_uri = "models:/{model_name}@production".format(model_name=model_name)
+    loaded_model = mlflow.pyfunc.load_model(model_uri=model_production_uri)
+    #loaded_model = load_model_with_tag("mlflow_tracking_examples", "stage", "production") 
     decoded_data = decode_input(input_data)
     prediction = loaded_model.predict(decoded_data)
     prediction_list = prediction.tolist()
